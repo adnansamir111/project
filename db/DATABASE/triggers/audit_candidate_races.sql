115f431fc2cf5a17e2e3b13c772b80d6024448ae
Adnan Samir <adnansamir700@gmail.com>
2026-03-12 00:55:14 +0600

    update directories

--- db/DATABASE/triggers/audit_candidate_races.sql
+++ db/DATABASE/triggers/audit_candidate_races.sql
@@ -1,2 +1,5 @@
 -- Trigger on table: candidate_races
-CREATE TRIGGER audit_candidate_races AFTER INSERT OR DELETE OR UPDATE ON candidate_races FOR EACH ROW EXECUTE FUNCTION trg_audit_generic();
+CREATE TRIGGER audit_candidate_races
+AFTER INSERT OR DELETE OR UPDATE ON candidate_races
+FOR EACH ROW EXECUTE 
+FUNCTION trg_audit_generic();
